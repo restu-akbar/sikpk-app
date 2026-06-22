0a5248a61f33ca6182ea9c640f204a1011eef206
haijuf <muhammad.haikal.tif23@polban.ac.id>
2026-06-22 16:27:05 +0700

    fix: error deployment

--- docker/azure/entrypoint.sh
+++ docker/azure/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo "Membuat cache konfigurasi Laravel menggunakan environment runtime..."
 php artisan config:cache
 php artisan route:cache || true
 php artisan view:cache
@@ -8,5 +7,4 @@ php artisan event:cache || true
 
 php artisan migrate --force
 
-echo "Memulai aplikasi..."
 exec "$@"
